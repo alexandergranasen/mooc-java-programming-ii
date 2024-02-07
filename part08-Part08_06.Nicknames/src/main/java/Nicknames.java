cba7bd656fda3cd2f50c7705345373e80b0b415c
alexandergranasen <alexander.granasen@gmail.com>
2024-02-07 15:49:41 +0100

    Completed part08,exercise06

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -25,7 +25,12 @@ public class Nicknames {
  */
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> nickNames = new HashMap<>();
+        nickNames.put("matthew", "matt");
+        nickNames.put("michael", "mix");
+        nickNames.put("arthur", "artie");
         
+        System.out.println(nickNames.get("matthew"));
     }
 
 }
